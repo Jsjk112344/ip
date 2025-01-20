2524d1f968f25e03dea7284798ec79ec210fd0f7
Justin Soon <jienkhye01@gmail.com>
2025-01-21 00:36:19 +0800

    completed level 2 add lists

--- src/main/java/Hirono.java
+++ src/main/java/Hirono.java
@@ -1,7 +1,15 @@
 import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Hirono {
-    public static String line = "--------------------------------------------------";
+    public static String line = "\n--------------------------------------------------";
+    public static void printHashMap(HashMap<Integer, String> map) {
+        for (Map.Entry<Integer, String> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + ". " + entry.getValue());
+        }
+    }
+
     public static void main(String[] args) throws IOException{
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         PrintWriter writer = new PrintWriter(System.out);
@@ -23,23 +31,31 @@ public class Hirono {
         writer.println(line);
 
         writer.println("Hello! I'm Hirono");
-        writer.println("What can I do for you?\n");
+        writer.println("What can I do for you?");
 
         writer.println(line);
-        writer.println("What would you like me to echo?\n");
+        writer.println("What would you like to add to the list?");
         writer.flush();
         String input = reader.readLine();
+        Integer itemCounter = 1;
+        HashMap<Integer, String> items = new HashMap<>();
 
         while(!input.equals("bye")) {
             writer.println(line);
-            writer.println(input);
-            writer.println(line);
-            writer.println("What would you like me to echo next?\n");
+            if (input.equals("list")) {
+                printHashMap(items);
+
+            } else {
+                writer.println("added: " + input);
+                writer.println(line);
+                items.put(itemCounter, input);
+                itemCounter++;
+            }
             writer.flush();
             input = reader.readLine();
         }
         writer.println(line);
-        writer.println("Bye. Hope to see you again soon!\n");
+        writer.println("Bye. Hope to see you again soon!");
         writer.println(line + "\n");
 
         
