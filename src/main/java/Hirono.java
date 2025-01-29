46dfdc8beb588516b3c5f77b4ba62f9489408779
Justin Soon <jienkhye01@gmail.com>
2025-01-29 23:59:54 +0800

    Added a Parser Class

--- src/main/java/Hirono.java
+++ src/main/java/Hirono.java
@@ -4,42 +4,26 @@ public class Hirono {
     public static String line = "\n--------------------------------------------------";
 
     public static void main(String[] args) throws IOException, HironoException {
-        Ui ui = new Ui(); 
+        Ui ui = new Ui();
         Storage storage = new Storage("./data/hirono.txt");
+        Parser parser = new Parser();
         TaskList taskList;
+
         ui.showWelcome();
-        taskList = storage.loadTasks();
+
+        // Load tasks
+        try {
+            taskList = storage.loadTasks();
+            ui.showMessage("Tasks loaded successfully!");
+        } catch (IOException e) {
+            ui.showMessage("Error loading tasks. Starting with an empty task list.");
+            taskList = new TaskList();
+        }
 
         String input = ui.readCommand();
-        while (!input.equals("bye")) {
+        while (!parser.isExitCommand(input)) {
             try {
-                if (input.equals("list")) {
-                    taskList.listTasks();
-                } else if (input.startsWith("delete")) {
-                    int taskId = parseTaskId(input, "delete");
-                    storage.deleteTask(taskId);
-                    ui.showMessage(taskList.deleteTask(taskId));
-                } else if (input.startsWith("mark")) {
-                    int taskId = parseTaskId(input, "mark");
-                    taskList.markTask(taskId);
-                    ui.showMessage("Task " + taskId + " marked as done.");
-                } else if (input.startsWith("unmark")) {
-                    int taskId = parseTaskId(input, "unmark");
-                    taskList.unmarkTask(taskId);
-                    ui.showMessage("Task " + taskId + " marked as not done.");
-                } else if (input.startsWith("todo")) {
-                    ui.showMessage(taskList.addTask(input, "todo"));
-                } else if (input.startsWith("deadline")) {
-                    ui.showMessage(taskList.addTask(input, "deadline"));
-                } else if (input.startsWith("event")) {
-                    ui.showMessage(taskList.addTask(input, "event"));
-                } else if (input.startsWith("date")) {
-                    ui.showMessage(taskList.getEventsOnDate(input));
-                } else {
-                    throw new HironoException("I'm sorry, but I don't know what that means.");
-                }
-                storage.saveTasks(taskList);
-
+                parser.handleCommand(input, taskList, ui, storage);
             } catch (HironoException e) {
                 ui.showError(e.getMessage());
             } catch (NumberFormatException e) {
@@ -49,15 +33,7 @@ public class Hirono {
             ui.showDivider();
             input = ui.readCommand();
         }
-        
-        ui.showGoodbye();
-    }
 
-    private static int parseTaskId(String input, String command) throws HironoException {
-        String[] parts = input.split(" ");
-        if (parts.length < 2) {
-            throw new HironoException("The " + command + " command requires a task ID.");
-        }
-        return Integer.parseInt(parts[1]);
+        ui.showGoodbye();
     }
 }
---
+++ src/main/java/Parser.java
@@ -0,0 +1,47 @@
+
+import java.io.IOException;
+
+public class Parser {
+
+    public boolean isExitCommand(String input) {
+        return input.equals("bye");
+    }
+
+    public void handleCommand(String input, TaskList taskList, Ui ui, Storage storage) throws HironoException, IOException {
+        if (input.equals("list")) {
+            taskList.listTasks();
+        } else if (input.startsWith("delete")) {
+            int taskId = parseTaskId(input, "delete");
+            storage.deleteTask(taskId);
+            ui.showMessage(taskList.deleteTask(taskId));
+        } else if (input.startsWith("mark")) {
+            int taskId = parseTaskId(input, "mark");
+            taskList.markTask(taskId);
+            ui.showMessage("Task " + taskId + " marked as done.");
+        } else if (input.startsWith("unmark")) {
+            int taskId = parseTaskId(input, "unmark");
+            taskList.unmarkTask(taskId);
+            ui.showMessage("Task " + taskId + " marked as not done.");
+        } else if (input.startsWith("todo")) {
+            ui.showMessage(taskList.addTask(input, "todo"));
+        } else if (input.startsWith("deadline")) {
+            ui.showMessage(taskList.addTask(input, "deadline"));
+        } else if (input.startsWith("event")) {
+            ui.showMessage(taskList.addTask(input, "event"));
+        } else if (input.startsWith("date")) {
+            ui.showMessage(taskList.getEventsOnDate(input));
+        } else {
+            throw new HironoException("I'm sorry, but I don't know what that means.");
+        }
+
+        storage.saveTasks(taskList);
+    }
+
+    private int parseTaskId(String input, String command) throws HironoException {
+        String[] parts = input.split(" ");
+        if (parts.length < 2) {
+            throw new HironoException("The " + command + " command requires a task ID.");
+        }
+        return Integer.parseInt(parts[1]);
+    }
+}
